6d2bef886cebd10e53845b6845287034109884fe
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2023-09-24 09:12:13 -0400

    Api folder refactored

---
+++ lib/src/api/api.dart
@@ -0,0 +1,52 @@
+class OpenWeatherMapAPI {
+  OpenWeatherMapAPI(this.apiKey);
+  final String apiKey;
+
+  static const String _apiBaseUrl = "api.openweathermap.org";
+  static const String _apiPath = "/data/2.5/";
+
+  Uri weatherByCity(String city) => _buildUri(
+        endpoint: "weather",
+        parameters: cityQueryParameters(city),
+      );
+
+  Uri weatherByLatLon(double lat, double lon) => _buildUri(
+        endpoint: "weather",
+        parameters: latLonQueryParameters(lat, lon),
+      );
+
+  // Uri forecastByCity(String city) => _buildUri(
+  //       endpoint: "forecast",
+  //       parameters: cityQueryParameters(city),
+  //     );
+
+  // Uri forecastByLatLon(double lat, double lon) => _buildUri(
+  //       endpoint: "forecast",
+  //       parameters: latLonQueryParameters(lat, lon),
+  //     );
+
+  Uri _buildUri({
+    required String endpoint,
+    required Map<String, dynamic> parameters,
+  }) {
+    return Uri(
+      scheme: "https",
+      host: _apiBaseUrl,
+      path: "$_apiPath$endpoint",
+      queryParameters: parameters,
+    );
+  }
+
+  Map<String, dynamic> cityQueryParameters(String city) => {
+        "q": city,
+        "appid": apiKey,
+        "units": "metric",
+      };
+
+  Map<String, dynamic> latLonQueryParameters(double lat, double lon) => {
+        "lat": lat.toString(),
+        "lon": lon.toString(),
+        "appid": apiKey,
+        "units": "metric",
+      };
+}
