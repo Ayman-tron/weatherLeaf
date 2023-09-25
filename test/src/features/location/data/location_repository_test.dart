cffb76b02b8c3fca884aae907842e55ad3268947
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2023-09-25 11:53:01 -0400

    updated latLonQueryParameters(Location location)

---
+++ test/src/features/location/data/location_repository_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockGeolocator extends Mock implements Geolocator {}
+
+void main() {
+  test('location repository ...', () async {});
+}
