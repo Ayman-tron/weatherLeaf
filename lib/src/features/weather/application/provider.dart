cdd19abbbcbb9201d51faa5e40965f02bec38b65
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2023-10-29 17:18:53 -0400

    hourlyWeatherProvider added

--- lib/src/features/weather/application/provider.dart
+++ lib/src/features/weather/application/provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weatherLeaf/src/features/location/data/location_repository.dart';
 import 'package:weatherLeaf/src/features/location/domain/location.dart';
 import 'package:weatherLeaf/src/features/weather/data/weather_repository.dart';
+import 'package:weatherLeaf/src/features/weather/domain/forecast.dart';
 import 'package:weatherLeaf/src/features/weather/domain/weather.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'provider.g.dart';
@@ -32,6 +33,13 @@ Future<Weather> weatherByLatLon(
       .getWeatherByLatLon(location: location);
   return weather;
 }
+
+final hourlyWeatherProvider = FutureProvider.autoDispose<Forecast>((ref) async {
+  final city = ref.watch(cityProvider);
+  final forecast =
+      await ref.watch(weatherRepositoryProvider).getForecastByCity(city: city);
+  return forecast;
+});
 
 @riverpod
 Future<Weather> userLocationAndCity(UserLocationAndCityRef ref) async {
