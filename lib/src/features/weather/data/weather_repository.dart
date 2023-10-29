b3d58f99dc956c246d2682877df16f7432c513d3
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2023-10-29 17:15:10 -0400

    Methods to obtain forecast added

--- lib/src/features/weather/data/weather_repository.dart
+++ lib/src/features/weather/data/weather_repository.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:weatherLeaf/src/api/api.dart';
 import 'package:weatherLeaf/src/api/api_keys.dart';
 import 'package:weatherLeaf/src/features/location/domain/location.dart';
+import 'package:weatherLeaf/src/features/weather/domain/forecast.dart';
 import 'package:weatherLeaf/src/features/weather/domain/weather.dart';
 // Credit: The following code snippet is adapted from the Original Project
 // (https://github.com/bizz84/open_weather_example_flutter/tree/main) by Andrea Bizztto (GitHub: @bizz84).
@@ -24,6 +25,15 @@ class HttpWeatherRepository {
   Future<Weather> getWeatherByLatLon({required Location location}) => _getData(
         uri: api.weatherByLatLon(location),
         builder: (data) => Weather.fromJson(data),
+      );
+  Future<Forecast> getForecastByCity({required String city}) => _getData(
+        uri: api.forecastByCity(city),
+        builder: (data) => Forecast.fromJson(data),
+      );
+  Future<Forecast> getForecastByLatLon({required Location location}) =>
+      _getData(
+        uri: api.forecastByLatLon(location),
+        builder: (data) => Forecast.fromJson(data),
       );
 
   Future<T> _getData<T>({
