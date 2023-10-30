87c018f49c58c7c4c6bd32bc040f440357ddd33a
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2023-10-29 21:08:14 -0400

    assests refactored

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Weather App',
+      title: 'Weather Leaf',
       debugShowCheckedModeBanner: false,
       routerConfig: goRouter,
       restorationScopeId: 'app',
